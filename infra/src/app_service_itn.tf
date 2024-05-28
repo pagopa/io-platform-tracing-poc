ae576e40a7956d83049131edd8ba112ecc45523c
AleDore <alessio85.dore@gmail.com>
2024-05-28 13:45:54 +0200

    add HEAP_DUMP_ACTIVE env prop

--- infra/src/app_service_itn.tf
+++ infra/src/app_service_itn.tf
@@ -10,6 +10,7 @@ locals {
     FN_CLIENT_BASE_URL = "https://${module.function_app_itn.default_hostname}"
     FN_CLIENT_KEY = module.function_app_itn.master_key
 
+    HEAP_DUMP_ACTIVE              = false
     HEAP_CONTAINER_NAME           = "heapdump"
     HEAP_DUMP_STORAGE_CONN_STRING = data.azurerm_storage_account.fnadmintest.primary_connection_string
 
